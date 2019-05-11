c39682e06ca9bd1b0ea990ccb1160254629bf4b5
TSI\Shankar <shankar.ganesan@tpfsoftware.com>
2019-05-11 23:59:16 +0530

    completed CREATE, LIST, DELETE for basic contact management

--- manager/app/controllers/contact.controller.js
+++ manager/app/controllers/contact.controller.js
@@ -2,7 +2,6 @@ const Contact = require("../models/contact.model");
 
 //Create and Save a new contact
 exports.create = (req, res) => {
-  console.log(req.body);
   // Validate the request
   if (!req.body) {
     return res.status(400).send({
@@ -26,13 +25,65 @@ exports.create = (req, res) => {
 };
 
 // Retrieve and list all contacts from DB
-exports.findAll = (req, res) => {};
+exports.findAll = (req, res) => {
+  Contact.find()
+    .then(contacts => {
+      res.send(contacts);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Error occurred while trying to retrieve the contacts"
+      });
+    });
+};
 
 //Find a single contact with contactId
-exports.findOne = (req, res) => {};
+exports.findOne = (req, res) => {
+  Contact.findById(req.params.contactId)
+    .then(contact => {
+      if (!contact) {
+        return res.status(404).send({
+          message: `Contact not found with id --> ${req.params.contactId}`
+        });
+      }
+      res.send(contact);
+    })
+    .catch(error => {
+      if (error.kind === "ObjectId") {
+        return res.status(404).send({
+          message: `Contact not found with id --> ${req.params.contactId}`
+        });
+      }
+      return res.status(500).send({
+        message: `Error retriving contact with id --> ${req.params.contactId}`
+      });
+    });
+};
 
 //Update a contact with the contactId
 exports.update = (req, res) => {};
 
 //Delete a contact with the specified contactId
-exports.delete = (req, res) => {};
+exports.delete = (req, res) => {
+  Contact.findByIdAndRemove(req.params.contactId)
+    .then(contact => {
+      if (!contact) {
+        return res.status(404).send({
+          message: `Contact not found with id --> ${req.params.contactId}`
+        });
+      }
+      res.send({ message: `Contact Deleted successfully` });
+    })
+    .catch(error => {
+      if (error.kind === "ObjectId" || error.name === "NotFound") {
+        return res.status(404).send({
+          message: `Contact not found with id --> ${req.params.contactId}`
+        });
+      }
+
+      return res.status(500).send({
+        message: `Couldn't delete with id --> ${req.params.contactId}`
+      });
+    });
+};
